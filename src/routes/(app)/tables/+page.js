d02f0f9d515161c8ccda235854672d551846be91
xiwang <daring@outlook.com>
2024-02-26 14:14:20 +0800

    add pages

--- src/routes/(app)/tables/+page.js
+++ src/routes/(app)/tables/+page.js
@@ -1,8 +1,10 @@
+import { invoices } from '$lib/data/invoice';
+
 export const ssr = false;
 
 /** @type {import('./$types').PageLoad} */
 export function load() {
-    return {}
+    return {
+        invoices: invoices
+    }
 }
-
-
--- src/routes/(app)/tables/+page.svelte
+++ src/routes/(app)/tables/+page.svelte
@@ -1,4 +1,39 @@
 <script>
+	import Card from '$components/Card.svelte';
 	/** @type {import('./$types').PageData} */
 	export let data;
+	const invoices = data.invoices;
 </script>
+
+<Card type="border">
+	<h3 class="font-semibold text-lg">Invoices</h3>
+	<div class="mt-5 overflow-x-auto h-76">
+		<table class="table table-sm table-pin-rows table-pin-cols">
+			<thead>
+				<tr>
+					<th>Invoice</th>
+					<th>Status</th>
+					<th>Method</th>
+					<th class="text-right">Amount</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each invoices as invoice}
+					<tr class="odd:bg-white even:bg-slate-50">
+						<td>{invoice.invoice}</td>
+						<td>{invoice.paymentStatus}</td>
+						<td>{invoice.paymentMethod}</td>
+						<td class="text-right">{invoice.totalAmount}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+</Card>
+
+<Card type="border" class="mt-5">
+	<h3 class="font-semibold text-lg">Data tables</h3>
+	<div class="mt-5 overflow-x-auto h-76">
+		<table class="table table-sm table-pin-rows table-pin-cols"></table>
+	</div>
+</Card>
